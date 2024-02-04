0ccef354ac6d7fc1f1c0b0e5f17d2951cafed685
Serednia Anastasia <anastastasiaseredna.gmail.com>
2024-02-04 11:05:57 +0100

    temp commit

--- app/js/main.js
+++ app/js/main.js
@@ -85,12 +85,10 @@ $(function () {
     onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
-
     },
     onChange: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
-
     },
 
   });
